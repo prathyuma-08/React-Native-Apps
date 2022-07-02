438ca7536f300a75231fc6b797e433358083db4b
Prathyuma V <prathyuma.v2020@vitstudent.ac.in>
2022-07-02 11:49:01 +0530

    Updated Settings Section

--- components/Profile.js
+++ components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import colors from '../assets/colors/colors';
 import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -11,19 +11,68 @@ export default function Profile() {
                 <Text style={styles.header}>PROFILE</Text>
             </View>
             <View style={styles.contentWrapper}>
-            <View style={styles.nameWrapper}>
-                <Text style={styles.subhead}>Abhinav S</Text>
-                <Text style={styles.subhead}>29</Text>
-                <Text style={styles.subhead}>West Bengal</Text>
-                <Text style={styles.subhead}>India</Text>
-            </View>
-            <Image source={require('../assets/images/person.png')} style={styles.profile}/>
+                <View style={styles.nameWrapper}>
+                    <Text style={styles.subhead}>Abhinav S</Text>
+                    <Text style={styles.subhead}>29</Text>
+                    <Text style={styles.subhead}>West Bengal</Text>
+                    <Text style={styles.subhead}>India</Text>
+                </View>
+                <Image source={require('../assets/images/person.png')} style={styles.profile} />
             </View>
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>Edit profile</Text>
-                <Entypo name="edit" size={20} color={colors.white}/>
+                <Entypo name="edit" size={20} color={colors.white} />
             </TouchableOpacity>
-            <View style={styles.divider}/>
+            <View style={styles.divider} />
+            <View style={styles.settingsWrapper}>
+                <View style={styles.heading}>
+                    <Text style={styles.header}>SETTINGS</Text>
+                </View>
+                <View style={styles.itemsWrapper}>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Manage Permissions</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Notifications</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Payment Method</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Privacy Policy</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Terms of Service</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Rate Us</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                    <View style={styles.itemdivider}>
+                        <View style={styles.item}>
+                            <Text style={styles.itemText}>Customer Service</Text>
+                            <Entypo name="chevron-right" size={23} color={colors.orange} />
+                        </View>
+                    </View>
+                </View>
+            </View>
         </View>
     );
 }
@@ -43,22 +92,22 @@ const styles = StyleSheet.create({
         fontFamily: 'Lato-Bold',
         fontSize: 28
     },
-    contentWrapper:{
+    contentWrapper: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginHorizontal: 40
     },
-    profile:{
+    profile: {
         height: 110,
         width: 100
     },
-    subhead:{
+    subhead: {
         fontFamily: 'Lato-Regular',
         fontSize: 20,
         color: colors.black,
         marginBottom: 5
     },
-    button:{
+    button: {
         backgroundColor: colors.orange,
         flexDirection: 'row',
         justifyContent: 'center',
@@ -67,17 +116,42 @@ const styles = StyleSheet.create({
         marginHorizontal: 40,
         marginTop: 10
     },
-    buttonText:{
+    buttonText: {
         fontFamily: 'Lato-Bold',
         color: colors.white,
         fontSize: 20,
         padding: 10
     },
-    divider:{
+    divider: {
         height: 0.6,
         backgroundColor: colors.darkGray,
         marginTop: 10,
         marginHorizontal: 20
+    },
+    settingsWrapper: {
+        marginHorizontal: 20
+    },
+    heading: {
+        marginTop: 20
+    },
+    itemsWrapper: {
+        marginHorizontal: 10,
+    },
+    itemText: {
+        fontFamily: 'Lato-Regular',
+        fontSize: 18,
+        color: colors.darkGray,
+    },
+    itemdivider:{
+        borderBottomWidth: 0.5,
+        borderColor: colors.darkGray,
+        padding: 3
+    },
+    item:{
+        marginTop: 15,
+        flexDirection: 'row', 
+        justifyContent: 'space-between', 
+        alignItems: 'center'
     }
 }
 )
